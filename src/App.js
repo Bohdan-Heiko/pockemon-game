c047a79596acb46165147ba3271f9b5decd016d9
Bohdan <bgejko@gmaol.com>
2021-06-05 15:05:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Layout from './components/Layout';
+
+import imgBg2 from './Assets/bg2.jpg'
+import imgBg3 from './Assets/bg3.jpg'
+
 import './App.css';
 
 function App() {
   return (
-    <div>Hello</div>
+    <>
+      <Header
+        title={'This is title'}
+        desc={'This is Description!'}
+      />
+      <Layout
+        urlBg={imgBg2}
+      />
+      <Layout 
+        clrBg='blue'
+      />
+      <Layout
+        urlBg={imgBg3}
+      />
+      <Footer />
+    </>
   );
 }
 
